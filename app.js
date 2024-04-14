84eb2df315440aa47265d92e36054faa864ebee4
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 10:34:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+//Solution # 11
+// Name store the names of a few of your friend in a array called names. print each persons name by accessing each element in the 
+// list, one at a time  
+let members = ["Adeel", "Aisha", "Aiza", "Fatima"];
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+//Solution # 11
+// Name store the names of a few of your friend in a array called names. print each persons name by accessing each element in the 
+// list, one at a time  
+
+
+let members: string[] = ["Adeel","Aisha","Aiza","Fatima"];
+for (let i=0; i<members.length; i++){
+    console.log(members[i])
+}
